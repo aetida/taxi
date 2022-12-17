07d69dd7dde586f07988e41fb1e1012f6396091d
dmitry <113114524+aetida@users.noreply.github.com>
2022-12-17 11:40:38 +0500

    интерфейс + комментарии

--- Ft-210007_Permyakov D.S..py
+++ Ft-210007_Permyakov D.S..py
@@ -1,48 +1,67 @@
-def sort_list():
+def sort_list(): #сортировка пузырьком тарифов и номеров такси
     for i in range(number_empl-1):
         for j in range(number_empl-i-1):
             if taxi_price[j] < taxi_price[j+1]:
                 taxi_price[j], taxi_price[j+1] = taxi_price[j+1], taxi_price[j]
                 number_taxi[j], number_taxi[j+1] = number_taxi[j+1],number_taxi[j]
-    print(' '.join(map(str, number_taxi)))
+    print(' '.join(map(str, number_taxi))) #вывод строки ответа
                
    
-def sort_way():
+def sort_way(): #сортировка пузырьком расстояний
     for i in range(number_empl-1):
         for j in range(number_empl-i-1):
             if way_dist[j] > way_dist[j+1]:
                  way_dist[j], way_dist[j+1] = way_dist[j+1], way_dist[j]           
 
-
-number_empl = int(input('Введите количество сотрудников: '))
-way_dist = []
+#обработка ввода количества сотрудников
+while True:
+	try:
+		number_empl = int(input('Введите количество сотрудников (от 1 до 1000): '))
+		if number_empl <1 or number_empl >1000:
+			print('некорректный ввод\nповторите попытку')
+		else:
+			break
+	except ValueError:
+		print('некорректный ввод\nповторите попытку')
+			
+way_dist = [] #создание переменных для расстояний, тарифов
 taxi_price = []
-number_taxi = []
-count_1 = 0
-count_2 = 0
-final_price = 0
-
-while count_1 < number_empl:
-    print('Введите расстояние для сотрудника №', count_1 + 1,':', end=' ')
-    way_dist.append(int(input()))
-    count_1 += 1
-
-while count_2 < number_empl:
-    print('Введите тариф такси №', count_2 + 1, ':', end=' ')
-    taxi_price.append(int(input()))
-    number_taxi.append(count_2+1)
-    count_2 += 1
+number_taxi = [i+1 for i in range(number_empl)] #заполнение списка номеров такси
+final_price = 0 #переменная для окончательной цены			
 
 
+for i in range(number_empl): # цикл обработки ввода расстояний
+    while True:
+        string=input(f'Расстояние до дома {i+1} сотрудника: ')
+        try:
+            way_dist.append(int(string))
+            break
+        except ValueError:
+            print('Ошибка ввода. Попробуйте еще раз')
+
+
+for i in range(number_empl): #цикл обработки ввода тарифов такси
+    while True: 
+        string = input(f'Тариф {i+1} такси: ')
+
+        try:
+            taxi_price.append(int(string))
+            break
+        except ValueError:
+            print ('некорректный ввод\nповторите попытку')
+
+#обращение к функциям сортировки списков
 sort_list()
 sort_way()
 
 
-for i in range(number_empl):
+for i in range(number_empl): #цикл расчета итоговой цены
     final_price += way_dist[i]*taxi_price[i]
 
-print('Общая стоимость: ', final_price)
+print('Общая стоимость: ', final_price) #цикл расчета итоговой цены
 
+#алгоритм перевода числа в буквенную запись
+#разбиение числа на разряды
 x = final_price//100000
 final_price = final_price - x*100000
 z = final_price//10000
@@ -55,18 +74,19 @@ b = final_price//10
 final_price = final_price - b*10
 n = final_price//1 
 
+#создание списков с буквенной записью
 hundred = ['сто ','двести ','триста ', 'четыреста ','пятьсот ','шестьсот ','семьсот ','восемьсот ','девятьсот ']
 dozens = ['десять ','одиннадцать ','двенадцать ','тринадцать ','четырнадцать ','пятнадцать ','шестнадцать ','семнадцать ','восемнадцать ','девятнадцать ', 'двадцать ','тридцать ','сорок ','пятьдесят ','шестдесят ','семдесят ','восемдесят ','девяносто ',''] 
 thousands = ['','тысяча ', 'тысячи ','тысяч ']
 unit = ['один ','два ','три ','четыре ','пять ','шесть ','семь ','восемь ','девять ','одна ','две ']
 rubl = ['рубль','рубля','рублей']
 
-if x != 0:
+if x != 0: # обработка разряда сотен тысяч
 	s = hundred[x-1]
 else:
 	s = thousands[0]
 
-if z != 0:
+if z != 0: # обработка разряда десятков тысяч и тысяч если первое не равно 0
 	if z == 1:
 		if c == 0:
 			d = dozens[0]
@@ -80,7 +100,7 @@ if z != 0:
 			f = unit[c-1]
 		else:
 			f=thousands[0]
-else:
+else: # обработка разряда тысяч если десятки тысяч равны 0
 	d = thousands[0]
 	if c != 0:
 		if c == 1:
@@ -92,7 +112,7 @@ else:
 	else:
 		f=thousands[0]
 
-if x!=0 or z!=0 or c!=0:
+if x!=0 or z!=0 or c!=0: #склонение слова "тысяча"
 	if z == 1 or (c!=2 and c!=3 and c!=4 ):
 		m = thousands[3]
 	else:
@@ -116,12 +136,12 @@ if x!=0 or z!=0 or c!=0:
 else:
 	m=thousands[0]
 
-if v != 0:
+if v != 0: # обработка разряда сотен
 	g = hundred[v-1]
 else:
 	g = thousands[0]
 
-if b != 0:
+if b != 0: # обработка разряда десятков и единиц если перве не равно 0
 	if b == 1:
 		if n == 0:
 			h = dozens[0]
@@ -135,14 +155,14 @@ if b != 0:
 			j = unit[n-1]
 		else:
 			j=thousands[0]
-else:
+else: # обработка разряда единиц если десятки равны 0
 	h = thousands[0]
 	if n != 0:
 		j = unit[n-1]
 	else:
 		j=thousands[0]
 
-if n == 1:
+if n == 1: # склонение рублей если число оканчивается на 1
 	if b!=1:
 		r=rubl[0]
 	else:
@@ -150,7 +170,7 @@ if n == 1:
 else:
 	pass
 
-if n == 2 or n == 3 or n ==4:
+if n == 2 or n == 3 or n ==4:  # склонение рублей если число оканчивается на 2 3 4
 	if b!=1:
 		r=rubl[1]
 	else:
@@ -158,9 +178,9 @@ if n == 2 or n == 3 or n ==4:
 else:
 	pass
 
-if n > 4 or n == 0:
+if n > 4 or n == 0: # склонение рублей при остальных случаях окончания
 	r = rubl[2]
 else:
 	pass
 
-print(str(s)+str(d)+str(f)+str(m)+str(g)+str(h)+str(j)+str(r))
+print(str(s)+str(d)+str(f)+str(m)+str(g)+str(h)+str(j)+str(r)) # вывод числа словами
